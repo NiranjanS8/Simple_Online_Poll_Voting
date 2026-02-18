2a45560e5cbfb6cdaca2e1bf2b0d4e09474be6d9
NiranjanS8 <niranjansankeshwari9@gmail.com>
2026-02-18 22:20:44 +0530

    Initial api + db design

---
+++ src/main/java/com/voting/votingapp/model/Poll.java
@@ -0,0 +1,27 @@
+package com.voting.votingapp.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Poll {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private  String question;
+
+    @ElementCollection    //create a different table in db
+    private List<String> options = new ArrayList<>();
+
+    @ElementCollection    //create a different table in db
+    private List<Long> votes = new ArrayList<>();
+
+}
---
+++ src/main/java/com/voting/votingapp/service/PollService.java
@@ -0,0 +1,19 @@
+package com.voting.votingapp.service;
+
+import com.voting.votingapp.model.Poll;
+import com.voting.votingapp.repository.PollRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PollService {
+
+    private final PollRepository pollRepository;
+
+    public PollService(PollRepository pollRepository) {
+        this.pollRepository = pollRepository;
+    }
+
+    public Poll createPoll(Poll poll) {
+        return pollRepository.save(poll);
+    }
+}
